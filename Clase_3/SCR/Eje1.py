b079e2536e31ab636531b8c9d1251b06a1063c03
Martin A Ruiz <ruizelectronica@gmail.com>
2023-07-29 16:05:18 -0300

    upload SCR folder and files

---
+++ Clase_3/SCR/Eje1.py
@@ -0,0 +1,11 @@
+""" Escribir un programa que le pida al usuario que ingrese su edad y muestre
+en pantalla si es mayor de edad (18 años) o no. """
+
+
+edad = int(input("put yor aye: "))
+
+if edad>18:
+    print("you are adult")
+else:
+    print("you are minor")
+
---
+++ Clase_3/SCR/Eje2.py
@@ -0,0 +1,9 @@
+""" Escribir un programa que le pida al usuario que ingrese un número y
+muestre en pantalla si es par o impar. """
+
+number = float(input("Input a number: "))
+
+if number % 2 == 0:
+    print("It is an even number.")  #Es un nomero par
+else:
+    print("It is an odd number.")   #Es un numero inpar
