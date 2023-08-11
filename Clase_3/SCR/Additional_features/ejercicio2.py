e9773ea9ceaa72fd4d65acd136aa5e428a0341ba
Martin A. Ruiz <ruizelectronica@gmail.com>
2023-08-11 19:35:06 -0300

    Done Exercise 2

--- Clase_3/SCR/Additional_features/ejercicio2.py
+++ Clase_3/SCR/Additional_features/ejercicio2.py
@@ -1,1 +1,8 @@
 # Escribir un programa que pida al usuario un número entero y muestre por pantalla si es par o impar.
+
+number = int(input("Input a number: "))
+
+if number % 2 == 0:
+    print("It is an even number.")  #Es un nomero par
+else:
+    print("It is an odd number.")   #Es un numero inpar
