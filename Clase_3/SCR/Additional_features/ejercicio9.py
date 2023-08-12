28ada7b6b72aa30066ea8eeb69213e1c8b5eecdd
Martin A. Ruiz <ruizelectronica@gmail.com>
2023-08-11 22:26:14 -0300

    Done Exercise 9

--- Clase_3/SCR/Additional_features/ejercicio9.py
+++ Clase_3/SCR/Additional_features/ejercicio9.py
@@ -3,3 +3,16 @@
 
 ##### Si hacen este son genios!!!! #####
 ##### Nota: Este es un poco mas complicado, pero con lo que vimos hasta la fecha, lo pueden resolver. ##### 
+
+
+
+verifie = input("Input a letter: ")
+vowel = ("a", "e", "i", "o", "u")
+if len(verifie) > 1:
+    print("Error: Please input a single letter")
+else:
+    verifie = verifie.lower()  
+    if verifie in vowel:  
+        print("The letter is a vowel")
+    else:
+        print("The letter isn't a vowel")
