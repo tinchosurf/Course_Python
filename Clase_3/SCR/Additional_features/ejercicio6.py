24ab597ae33c19565fa0422754b081b98f1d9150
Martin A. Ruiz <ruizelectronica@gmail.com>
2023-08-11 21:43:03 -0300

    Done Exercise 6

--- Clase_3/SCR/Additional_features/ejercicio6.py
+++ Clase_3/SCR/Additional_features/ejercicio6.py
@@ -1,3 +1,13 @@
-# Escribir un programa que pida al usuario dos números y devuelva su división. Si el usuario introduce el divisor cero debera imprimir un mensaje de error.
-
+# Escribir un programa que pida al usuario dos números y devuelva su división. 
+# Si el usuario introduce el divisor cero debera imprimir un mensaje de error.
 ##### Nota: Este es un poco mas complicado, pero con lo que vimos hasta la fecha, lo pueden resolver. #####
+
+
+number1 = float(input("Insert number one: "))
+number2 = float(input("Insert number two: "))
+
+if number2 == 0:
+    print("The numbers aren't divisible by zero")
+else:
+    total = number1 / number2
+    print(f"The numbers are divisible. The result is {total}")
