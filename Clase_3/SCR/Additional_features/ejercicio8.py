3528e7cf4a51d5f0a6c5a619580dd796084ff6bd
Martin A. Ruiz <ruizelectronica@gmail.com>
2023-08-11 22:04:18 -0300

    Done Exercise 8

--- Clase_3/SCR/Additional_features/ejercicio8.py
+++ Clase_3/SCR/Additional_features/ejercicio8.py
@@ -2,3 +2,12 @@
 
 ##### Nota: Este es un poco mas complicado, pero con lo que vimos hasta la fecha, lo pueden resolver. #####
 
+number1 = float(input("Insert number one: "))
+number2 = float(input("Insert number two: "))
+
+if number1 > number2:
+    print("Number 1 is greater than number 2")
+elif number1 < number2:
+    print("Number 2 is greater than number 1")
+else:
+    print("Number 1 is equal to number 2")
