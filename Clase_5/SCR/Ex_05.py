0c156fe85b8ac2e4eb2e21233531b3fdef07e114
Martin A Ruiz <ruizelectronica@gmail.com>
2023-09-21 22:24:44 -0300

    Add Ex_05.py file to Clase_5\SCR

---
+++ Clase_5/SCR/Ex_05.py
@@ -0,0 +1,12 @@
+# Imprime el siguiente patrón de asteriscos usando bucles for anidados.
+# * 
+# * * 
+# * * * 
+# * * * *
+# * * * * *
+s=""
+
+for i in range(0,5):
+    s=s+"* "
+    print(f"{s}")
+
