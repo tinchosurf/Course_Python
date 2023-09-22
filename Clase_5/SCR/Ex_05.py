2a46d1e964a615b573c2114f34da568b8985500b
Martin A Ruiz <ruizelectronica@gmail.com>
2023-09-21 22:33:06 -0300

    Modified Ex_05.py

--- Clase_5/SCR/Ex_05.py
+++ Clase_5/SCR/Ex_05.py
@@ -4,9 +4,13 @@
 # * * * 
 # * * * *
 # * * * * *
-s=""
+# s=""
 
-for i in range(0,5):
-    s=s+"* "
-    print(f"{s}")
+# for i in range(0,5):
+#     s=s+"* "
+#     print(f"{s}")
 
+for i in range(1, 6):
+    for j in range(i):
+        print("*", end=" ")
+    print()  # Agrega un salto de línea después de imprimir cada fila de asteriscos
